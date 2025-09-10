cdad0ca9defe23df3e6816530f81767974605f5c
Sergio <sergiodanilomosqueraquigua@gmail.com>
2025-09-10 11:53:50 -0500

    Agrega ejemplos básicos de sintaxis y operadores en JS
    Incluye archivo HTML y script con ejemplos de salida, entrada,
    declaración de variables, tipos de datos y operadores en JavaScript,
    orientados a introducción y práctica fundamental del lenguaje.

---
+++ javascript-1/js/javascrip.js
@@ -0,0 +1,44 @@
+//-------------Salida----------------
+//alert("hola");
+console.log("hola");
+//-------------Entrada----------------
+//let nombree = prompt("ingresa tu nombre");
+//console.log("Este es tu nombre: "+nombree);
+//-------------Variables----------------
+var nombre = "Checho";
+let apellido = "Mosquera";
+const altura = 1.70;
+console.log(nombre);
+console.log(apellido);
+console.log(altura);
+//-------------Tipos de datos----------------
+let array = [1,2,3,4,5];
+let edad = 22;
+console.log(edad);
+console.log(typeof edad);
+console.log(array);
+console.log(typeof array);
+//-------------Operadores----------------
+let numero1 = 10;
+let numero2 = 20;
+console.log((numero1 + numero2) + " suma");
+console.log((numero1 - numero2) + " resta");
+console.log((numero1 * numero2) + " multiplicacion");
+console.log((numero1 / numero2) + " division");
+console.log((numero1 % numero2) + " modulo");
+console.log((numero1 ** numero2) + " exponente");
+console.log(numero1++ + " incremento");
+console.log(numero2-- + " decremento");
+console.log(numero1 == " igualdad");
+console.log(numero1 === " igualdad estricta");
+console.log(numero1 != " desigualdad");
+console.log(numero1 !== " desigualdad estricta");
+console.log(numero1 > " mayor que");
+console.log(numero1 < " menor que");
+console.log(numero1 >= " mayor o igual que");
+console.log(numero1 <= " menor o igual que");
+//-------------
+let funcionAnonima = function(){
+    console.log("funcion anonima");
+}
+funcionAnonima();
